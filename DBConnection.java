c09693097de43800551029fb71984042dfef6107
fatihkaanbostan <fatih.bostan@ozu.edu.tr>
2026-05-05 11:20:57 +0300

    Create DBConnection.java
    DBConnection.java completed

---
+++ DBConnection.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/db_crs?allowPublicKeyRetrieval=true&useSSL=false";
+    private static final String USER = "root";
+    private static final String PASSWORD = "1907";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("MySQL Driver not found! Make sure to include mysql-connector-j in your classpath.");
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
