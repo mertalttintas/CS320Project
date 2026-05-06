4ef4a2a13b6ad7ddcbb203b47c91fc148e5ff3c4
cetinhosafci <cetin.hosafci@ozu.edu.tr>
2026-05-06 14:26:08 +0300

    Create Review class with review details

---
+++ Review.java
@@ -0,0 +1,10 @@
+import java.sql.Timestamp;
+
+public class Review {
+    public int reviewId;
+    public int vehicleId;
+    public int customerId;
+    public int rating;
+    public String comment;
+    public Timestamp reviewDate;
+}
