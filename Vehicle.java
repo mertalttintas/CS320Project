45c0e87a4ad6289ebd0c6eca47e8c75bf43de915
mertalttintas <mert.altintas1@yahoo.com>
2026-05-06 14:24:35 +0300

    Create Vehicle.java
    Vehicle class created

---
+++ Vehicle.java
@@ -0,0 +1,15 @@
+public class Vehicle {
+    public int vehicleId;
+    public int managerId;
+    public String brand;
+    public String model;
+    public String location;
+    public String fuelType;
+    public String transmission;
+    public int seats;
+    public double dailyPrice;
+    public int stockQuantity;
+    public String category;
+    public String status;
+    public String features;
+}
