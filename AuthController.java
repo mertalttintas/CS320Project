536dea7cde41fca35d4605514de19609d6a877e0
kaangonenli <kaan.gonenli@ozu.edu.tr>
2026-05-06 18:02:23 +0300

    Updated AuthController.java

--- AuthController.java
+++ AuthController.java
@@ -16,5 +16,21 @@ public class AuthController implements IBusinessLogic {
         if (user.isLocked) {
             throw new Exception("Account is locked due to 3 consecutive failed attempts.");
         }
+        if (checkPassword(password, user.password)) {
+            userDAO.resetFailedAttempts(user.userId);
+            currentUser = user;
+            return true;
+        } else {
+            userDAO.incrementFailedAttempts(user.userId);
+            // Re-fetch user to check updated failed attempts count
+            User updatedUser = userDAO.getUserByEmail(email);
+            if (updatedUser.failedAttempts >= 3) {
+                userDAO.lockAccount(user.userId);
+                throw new Exception("Account locked after 3 failed attempts.");
+            }
+            throw new Exception("Invalid email or password. Attempt " + updatedUser.failedAttempts + " of 3.");
+        }
     }
+    //Dear friends add remaining codes here
+
 }
