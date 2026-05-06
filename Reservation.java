b0bf33f53ecfa9c7d621dac77a667cc0e07be0fa
mertalttintas <mert.altintas1@yahoo.com>
2026-05-06 14:25:50 +0300

    Create Reservation.java
    Reservation class was added

---
+++ Reservation.java
@@ -0,0 +1,11 @@
+import java.sql.Date;
+
+public class Reservation {
+    public int reservationId;
+    public int customerId;
+    public int vehicleId;
+    public Date pickupDate;
+    public Date returnDate;
+    public double totalPrice;
+    public String status;
+}
